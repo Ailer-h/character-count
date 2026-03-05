0d72b4838998064dfb90d837952b20492685b881
Ailer-h <henriqueailer2@gmail.com>
2026-03-05 14:43:40 -0300

    Initial Commit

---
+++ counter.js
@@ -0,0 +1,23 @@
+let chars_label = document.getElementById("chars");
+let chars_no_spaces_label = document.getElementById("chars_no_spaces");
+let words_label = document.getElementById("words");
+let paragraphs_label = document.getElementById("paragraphs");
+
+let text_input = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", count_chars)
+text_input.addEventListener("input", count_chars)
+
+function count_chars(){
+    let text = text_input.value
+    
+    let chars = text.length
+    let chars_no_spaces = text.replace(/\s/g, "").length
+    let words = text.split(" ").length
+    let paragraphs = text.split("\n").length
+    
+    chars_label.textContent = "Characters: " + chars
+    chars_no_spaces_label.textContent = "No spaces: " + chars_no_spaces
+    words_label.textContent = "Words: " + words
+    paragraphs_label.textContent = "Paragraphs: " + paragraphs
+}
